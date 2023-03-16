ed577a47e17c5a0b637c4e7bd689845926053c47
Charles Matte <charles.matte@blaisindustries.com>
2023-03-16 11:33:35 -0400

    introduced leavetype service/repository

---
+++ LeaveManagement.Web/Contracts/IGenericRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace LeaveManagement.Web.Contracts
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T?> GetByIdAsync(int? id);
+        Task<List<T>> GetAllAsync();
+        Task<bool> Exists(int id);
+        Task<T> AddAsync(T entity);
+        Task<T> UpdateAsync(T entity);
+        Task DeleteAsync(int id);
+    }
+}
---
+++ LeaveManagement.Web/Repositories/GenericRepository.cs
@@ -0,0 +1,62 @@
+﻿using LeaveManagement.Web.Contracts;
+using LeaveManagement.Web.Data;
+using LeaveManagement.Web.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LeaveManagement.Web.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GenericRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<T?> GetByIdAsync(int? id)
+        {
+            if (id == null)
+            {
+                return null;
+            }
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public async Task<List<T>> GetAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+
+        public async Task<bool> Exists(int id)
+        {
+            var entity = await GetByIdAsync(id);
+            return entity != null;
+        }
+
+        public async Task<T> AddAsync(T entity)
+        {
+            await _context.AddAsync(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<T> UpdateAsync(T entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+            //_context.Update(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var entity = await GetByIdAsync(id);
+            if (entity != null)
+            {
+                _context.Set<T>().Remove(entity);
+            }
+
+            await _context.SaveChangesAsync();
+        }
+    }
+}
